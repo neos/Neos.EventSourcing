9c358c858a74e68689c1da55868e4c9e3ccb9651
Dominique Feyer <dfeyer@ttree.ch>
2016-08-13 23:44:32 +0200

    TASK: EventHandlerLocator based on annotations
    Add CompileStatic support to improve performance

--- Classes/Event/EventHandlerLocator.php
+++ Classes/Event/EventHandlerLocator.php
@@ -7,10 +7,12 @@ namespace Flowpack\Cqrs\Event;
  * (c) Hand crafted with love in each details by medialib.tv
  */
 
+use Flowpack\Cqrs\Annotations\EventHandler;
 use Flowpack\Cqrs\Event\Exception\EventBusException;
 use Flowpack\Cqrs\Message\MessageInterface;
 use TYPO3\Flow\Annotations as Flow;
 use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\Flow\Reflection\ReflectionService;
 
 /**
  * EventHandlerLocator
@@ -31,6 +33,15 @@ class EventHandlerLocator implements EventHandlerLocatorInterface
     protected $map = [];
 
     /**
+     * Register event handlers based on annotations
+     */
+    public function initializeObject()
+    {
+        $handlers = self::loadHandlers($this->objectManager);
+        $this->map = array_merge($handlers, $this->map);
+    }
+
+    /**
      * @param string $eventName
      * @param string $handlerName
      * @throws EventBusException
@@ -39,11 +50,12 @@ class EventHandlerLocator implements EventHandlerLocatorInterface
     {
         if (!$this->objectManager->isRegistered($handlerName)) {
             throw new EventBusException(sprintf(
-                "Event handler '%s' cannot be found by locator", $handlerName
+                "Event handler '%s' is not a registred object", $handlerName
             ));
         }
 
-        $this->map[$eventName][] = $handlerName;
+        $handlerHash = md5($handlerName);
+        $this->map[$eventName][$handlerHash] = $handlerName;
     }
 
     /**
@@ -64,6 +76,23 @@ class EventHandlerLocator implements EventHandlerLocatorInterface
             $handlers[] = $this->objectManager->get($handlerName);
         }
 
+        return $handlers;
+    }
+
+    /**
+     * @param ObjectManagerInterface $objectManager
+     * @return array
+     */
+    protected static function loadHandlers(ObjectManagerInterface $objectManager)
+    {
+        $handlers = [];
+        /** @var ReflectionService $reflectionService */
+        $reflectionService = $objectManager->get(ReflectionService::class);
+        foreach ($reflectionService->getClassNamesByAnnotation(EventHandler::class) as $handler) {
+            /** @var EventHandler $annotation */
+            $annotation = $reflectionService->getClassAnnotation($handler, EventHandler::class);
+            $handlers[$annotation->event][md5($handler)] = $handler;
+        }
         return $handlers;
     }
 }
