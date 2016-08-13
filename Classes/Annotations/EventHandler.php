2227594600b2a0f444fa1d4b1d95150c66993317
Dominique Feyer <dfeyer@ttree.ch>
2016-08-13 23:40:56 +0200

    TASK: Add EventHandler annotation

---
+++ Classes/Annotations/EventHandler.php
@@ -0,0 +1,35 @@
+<?php
+namespace Flowpack\Cqrs\Annotations;
+
+/*
+ * This file is part of the Medialib.Storage package.
+ *
+ * (c) Hand crafted with love in each details by medialib.tv
+ */
+
+/**
+ * Used to enable property injection.
+ *
+ * Flow will build Dependency Injection code for the property and try
+ * to inject a value as specified by the var annotation.
+ *
+ * @Annotation
+ * @Target("CLASS")
+ */
+final class EventHandler
+{
+    /**
+     * @var string
+     */
+    public $event;
+
+    /**
+     * @param array $values
+     */
+    public function __construct(array $values)
+    {
+        if (isset($values['event'])) {
+            $this->event = (string)$values['event'];
+        }
+    }
+}
