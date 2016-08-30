42d66e2b9d74eb2ca67d28f9ab61e6ac937967f9
Dominique Feyer <dfeyer@ttree.ch>
2016-08-30 23:49:37 +0200

    TASK: Replace file doc header

--- Classes/Domain/Exception/AggregateRootNotFoundException.php
+++ Classes/Domain/Exception/AggregateRootNotFoundException.php
@@ -2,9 +2,13 @@
 namespace Ttree\Cqrs\Domain\Exception;
 
 /*
- * This file is part of the Ttree.Cqrs package.
+ * This file is part of the Neos.Cqrs package.
  *
- * (c) Hand crafted with love in each details by medialib.tv
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
  */
 
 use TYPO3\Flow\Exception;
--- Classes/RuntimeException.php
+++ Classes/RuntimeException.php
@@ -2,13 +2,14 @@
 namespace Ttree\Cqrs;
 
 /*
- * This file is part of the Ttree.Cqrs package.
+ * This file is part of the Neos.Cqrs package.
  *
- * (c) Hand crafted with love in each details by medialib.tv
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
  */
-
-use TYPO3\Flow\Annotations as Flow;
-use TYPO3\Flow\Exception;
 
 /**
  * RuntimeException
