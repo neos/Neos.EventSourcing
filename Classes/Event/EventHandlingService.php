9bcf62e81d854628c99aed20cbd36295bcfd907f
Dominique Feyer <dfeyer@ttree.ch>
2016-09-24 13:42:04 +0200

    TASK: Add a simple service to handle event
    This service is responsible to commit event to store and dispatch them to
    the event bus. It's the foundation where event enrichment service will hook
    in the process and it allow more flexibility to import event from external
    service

---
+++ Classes/Event/EventHandlingService.php
@@ -0,0 +1,51 @@
+<?php
+namespace Neos\Cqrs\Event;
+
+/*
+ * This file is part of the Neos.Cqrs package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Neos\EventStore\Event\Metadata;
+use Neos\EventStore\EventStore;
+use Neos\EventStore\EventStream;
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * EventTypeService
+ *
+ * @Flow\Scope("singleton")
+ */
+class EventHandlingService
+{
+    /**
+     * @var EventStore
+     * @Flow\Inject
+     */
+    protected $eventStore;
+
+    /**
+     * @var EventBus
+     * @Flow\Inject
+     */
+    protected $eventBus;
+
+    /**
+     * @param string $streamName
+     * @param EventStream $stream
+     * @return int commited version number
+     * @return int
+     */
+    public function publish(string $streamName, EventStream $stream) :int
+    {
+        return $this->eventStore->commit($streamName, $stream, function (EventTransport $eventTransport, int $version) {
+            $eventTransport->getMetaData()->add(Metadata::VERSION, $version);
+            $this->eventBus->handle($eventTransport);
+        });
+    }
+}
