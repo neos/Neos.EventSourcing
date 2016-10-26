0449e510fe3d1cf46cc70e4533ea59a4a8045516
Bastian <b.waidelich@wwwision.de>
2016-10-26 12:47:32 +0200

    TASK: Use the event class name to resolve listener
    Previously the "event type" was used to resolve the `when*()` method names
    of event listeners.
    Currently the event type is equal to the short event class name, but this
    had some drawbacks:
    
    * We need an instance of the `EventTypeResolver` in the aggregate
    * It wasn't possible to map an event type to a different method
    
    This commit also fixes the package key prefixing!
    
    Resolves: #63

--- Classes/Domain/AbstractAggregateRoot.php
+++ Classes/Domain/AbstractAggregateRoot.php
@@ -14,7 +14,6 @@ namespace Neos\Cqrs\Domain;
 use Neos\Cqrs\Event\AggregateEventInterface;
 use Neos\Cqrs\Event\EventInterface;
 use Neos\Cqrs\Event\EventTransport;
-use Neos\Cqrs\Event\EventTypeResolver;
 use Neos\Cqrs\Message\MessageMetadata;
 use TYPO3\Flow\Annotations as Flow;
 
@@ -23,11 +22,6 @@ use TYPO3\Flow\Annotations as Flow;
  */
 abstract class AbstractAggregateRoot implements AggregateRootInterface
 {
-    /**
-     * @var EventTypeResolver
-     * @Flow\Inject
-     */
-    protected $eventTypeService;
 
     /**
      * @var string
@@ -110,7 +104,7 @@ abstract class AbstractAggregateRoot implements AggregateRootInterface
      */
     final protected function apply(EventInterface $event)
     {
-        $method = sprintf('when%s', $this->eventTypeService->getEventShortType($event));
+        $method = sprintf('when%s', (new \ReflectionClass($event))->getShortName());
         if (method_exists($this, $method)) {
             $this->$method($event);
         }
