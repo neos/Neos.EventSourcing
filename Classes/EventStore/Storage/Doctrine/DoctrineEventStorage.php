neos/Neos.EventSourcing

commit 4ec28d36644acab4b21fc1db50903e6058c8b669

Tweak DoctrineEventStorage DBAL types

Classes/EventStore/Storage/Doctrine/DoctrineEventStorage.php
@@ -20,6 +20,7 @@ use Doctrine\DBAL\Schema\Comparator;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Schema\SchemaConfig;
 use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use Neos\Error\Messages\Error;
 use Neos\Error\Messages\Notice;
 use Neos\Error\Messages\Result;
@@ -327,25 +328,25 @@ class DoctrineEventStorage implements EventStorageInterface
         $table = $schema->createTable($this->eventTableName);
 
         // The monotonic sequence number
-        $table->addColumn('sequencenumber', Type::INTEGER, ['autoincrement' => true]);
+        $table->addColumn('sequencenumber', Types::INTEGER, ['autoincrement' => true]);
         // The stream name, usually in the format "<BoundedContext>:<StreamName>"
-        $table->addColumn('stream', Type::STRING, ['length' => 255]);
+        $table->addColumn('stream', Types::STRING, ['length' => 255]);
         // Version of the event in the respective stream
-        $table->addColumn('version', Type::BIGINT, ['unsigned' => true]);
+        $table->addColumn('version', Types::BIGINT, ['unsigned' => true]);
         // The event type in the format "<BoundedContext>:<EventType>"
-        $table->addColumn('type', Type::STRING, ['length' => 255]);
+        $table->addColumn('type', Types::STRING, ['length' => 255]);
         // The event payload as JSON
-        $table->addColumn('payload', Type::TEXT);
+        $table->addColumn('payload', Types::TEXT);
         // The event metadata as JSON
-        $table->addColumn('metadata', Type::TEXT);
+        $table->addColumn('metadata', Types::TEXT);
         // The unique event id, usually a UUID
-        $table->addColumn('id', Type::STRING, ['length' => 255]);
+        $table->addColumn('id', Types::STRING, ['length' => 255]);
         // An optional correlation id, usually a UUID
-        $table->addColumn('correlationidentifier', Type::STRING, ['length' => 255, 'notnull' => false]);
+        $table->addColumn('correlationidentifier', Types::STRING, ['length' => 255, 'notnull' => false]);
         // An optional causation id, usually a UUID
-        $table->addColumn('causationidentifier', Type::STRING, ['length' => 255, 'notnull' => false]);
+        $table->addColumn('causationidentifier', Types::STRING, ['length' => 255, 'notnull' => false]);
         // Timestamp of the the event publishing
-        $table->addColumn('recordedat', Type::DATETIME);
+        $table->addColumn('recordedat', Types::DATE_IMMUTABLE);
 
         $table->setPrimaryKey(['sequencenumber']);
         $table->addUniqueIndex(['id'], 'id_uniq');

commit 09283518957f9c2feb5977f1132dc0cdf0287d83

BUGFIX: Fix recordedAt column type for DoctrineEventStorage
Fixes a regression introduced with 4ec28d36644acab4b21fc1db50903e6058c8b669 that
led to the `recordedAt` column of published events to miss the time component.

Classes/EventStore/Storage/Doctrine/DoctrineEventStorage.php
@@ -351,7 +351,7 @@ class DoctrineEventStorage implements EventStorageInterface
         // An optional causation id, usually a UUID
         $table->addColumn('causationidentifier', Types::STRING, ['length' => 255, 'notnull' => false]);
         // Timestamp of the the event publishing
-        $table->addColumn('recordedat', Types::DATE_IMMUTABLE);
+        $table->addColumn('recordedat', Types::DATETIME_IMMUTABLE);
 
         $table->setPrimaryKey(['sequencenumber']);
         $table->addUniqueIndex(['id'], 'id_uniq');