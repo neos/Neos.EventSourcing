56b0f80d884a79037107998de41185b7cf77156b
bwaidelich <b.waidelich@wwwision.de>
2020-08-20 18:58:41 +0200

    BUGFIX: Fix unit tests
    * Add `setUp` return type hint to fix compatibilty with PhpUnit 9+
    * Fix "Only arrays and Traversables can be unpacked" error with PHP 7.3

--- Tests/Unit/EventStore/Storage/InMemory/InMemoryStreamIteratorTest.php
+++ Tests/Unit/EventStore/Storage/InMemory/InMemoryStreamIteratorTest.php
@@ -13,10 +13,7 @@ class InMemoryStreamIteratorTest extends UnitTestCase
      */
     private $iterator;
 
-    /**
-     * @throws
-     */
-    public function setUp()
+    public function setUp(): void
     {
         $this->iterator = new InMemoryStreamIterator();
         $this->iterator->setEventRecords([
@@ -45,7 +42,6 @@ class InMemoryStreamIteratorTest extends UnitTestCase
 
     /**
      * @test
-     * @throws
      */
     public function setEventRecordsRejectsInvalidDate(): void
     {
@@ -69,7 +65,6 @@ class InMemoryStreamIteratorTest extends UnitTestCase
 
     /**
      * @test
-     * @throws
      */
     public function canSetEventRecordsAndGetRawEvents(): void
     {
@@ -82,7 +77,6 @@ class InMemoryStreamIteratorTest extends UnitTestCase
 
     /**
      * @test
-     * @throws
      */
     public function providesIteratorFunctions(): void
     {
